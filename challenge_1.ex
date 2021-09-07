af42cd3bffa2d5b0a192f36cbd58b87ff7d8e566
Simon Tirant <simon.tirant@gmail.com>
2021-09-07 15:26:21 +0200

    minor improvements

--- challenge_1.ex
+++ challenge_1.ex
@@ -1,11 +1,15 @@
 defmodule Challenge1 do
+    @author "Simon"
     def ask() do
         name = IO.gets("Hi honey ! What's your name ?\n") |> String.trim()
 
         case name do
-            "Simon" -> IO.puts("Hop hop hop ... Me name's #{name} too !\nAre you me-self ?")
-            "" -> IO.puts("...\nThis is not a name, it's just an empty value ...\nAre you an emtpy person ?")
-            _ -> IO.puts("Greetings #{name} !")
+            @author -> 
+                IO.puts("Hop hop hop ... Me name's #{@author} too !\nAre you me-self ?")
+            "" -> 
+                IO.puts("...\nThis is not a name, it's just an empty value ...\nAre you an emtpy person ?")
+            _ -> 
+                IO.puts("Greetings #{name} !")
         end
     end
 end
